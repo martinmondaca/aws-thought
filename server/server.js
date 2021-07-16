e852d8a3b26c4c4c311005d49498ec72112600cc
Martin Mondaca <martin7mondaca@gmail.com>
2021-07-16 09:05:13 -0700

    added multer dependency and added imported route to server for uploading images

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const userRoutes = require('./routes/user-routes');
+const imageRoutes = require('./routes/image-upload');
 
 // express middleware, used to be bodyparser
 app.use(express.json());
@@ -13,6 +14,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 // app.use(require('./routes'));
 app.use('/api/', userRoutes);
+app.use('/api/', imageRoutes);
 
 // Start the API server
 app.listen(PORT, () =>
